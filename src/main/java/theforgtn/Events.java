8550ec609a0278966ad880a84e503e6757a912f1
AkosVR <akosvr@gmail.com>
2021-08-28 14:16:07 +0200

    Fixes

--- src/main/java/theforgtn/Events.java
+++ src/main/java/theforgtn/Events.java
@@ -2,6 +2,7 @@ package theforgtn;
 
 import java.util.HashMap;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,36 +12,36 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 
-import static org.bukkit.Bukkit.getLogger;
 
 public class Events implements Listener {
     // HashMap <= playerData
-    HashMap<String, Double> data = new HashMap<String, Double>();
+    private HashMap<String, Double> data = new HashMap<String, Double>();
     @EventHandler(priority = EventPriority.MONITOR)
     public void onMove(PlayerMoveEvent event) {
         org.bukkit.entity.Player player = event.getPlayer();
         // Main exclusions
         if(player.isGliding()) {
             // Chunk load section
-            if (100 < player.getLocation().getChunk().getInhabitedTime()){
+            if (50 < player.getLocation().getChunk().getInhabitedTime()){
                 data.put(player + "x", player.getLocation().getX());
                 data.put(player + "y", player.getLocation().getY());
                 data.put(player + "z", player.getLocation().getZ());
             } else {
-                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("Lassabban! Újonnan generált részhez érkeztél!"));
+                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("Lassabban! Túl gyorsan haladsz!"));
                 player.teleport(new Location(event.getPlayer().getWorld(), data.get(player + "x"), data.get(player + "y"), data.get(player + "z"), player.getLocation().getYaw(), player.getLocation().getPitch()));
             }
             // AntiCheat Section
-            if (3F < data.get(player + "deltaXZ") && data.get(player + "deltaXZ") == ((double) Math.sqrt(Math.pow(event.getTo().getX() - event.getFrom().getX(), 2) + Math.pow(event.getTo().getZ() - event.getFrom().getZ(), 2)))) {
+            if (2.2F < data.get(player + "deltaXZ") && data.get(player + "deltaXZ") == ((double) Math.sqrt(Math.pow(event.getTo().getX() - event.getFrom().getX(), 2) + Math.pow(event.getTo().getZ() - event.getFrom().getZ(), 2)))) {
                 player.teleport(new Location(event.getPlayer().getWorld(), data.get(player + "setback_x"), data.get(player + "setback_y"), data.get(player + "setback_z"), player.getLocation().getYaw(), player.getLocation().getPitch()));
                 player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText("Lassabban! Túl gyorsan haladsz!"));
-            } else if(data.get(player + "deltaXZ") < 3F){
+            } else if(data.get(player + "deltaXZ") < 2.2F){
                 data.put(player + "setback_x", player.getLocation().getX());
                 data.put(player + "setback_y", player.getLocation().getY());
                 data.put(player + "setback_z", player.getLocation().getZ());
             }
             // Last tick data
             data.put(player + "deltaXZ", (double) Math.sqrt(Math.pow(event.getTo().getX() - event.getFrom().getX(), 2) + Math.pow(event.getTo().getZ() - event.getFrom().getZ(), 2)));
+
         }
     }
     @EventHandler(priority = EventPriority.MONITOR)
@@ -53,5 +54,6 @@ public class Events implements Listener {
         data.put(player + "setback_y", player.getLocation().getY());
         data.put(player + "setback_z", player.getLocation().getZ());
         data.put(player + "deltaXZ", 0.0);
+        data.put(player + "lastDeltaY", 0.0);
     }
 }
--- src/main/java/theforgtn/Main.java
+++ src/main/java/theforgtn/Main.java
@@ -1,19 +1,34 @@
 package theforgtn;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 
 public final class Main extends JavaPlugin {
+    // Startup logics
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new Events(), this);
         getLogger().info("AntiElytraLag enabled!");
     }
+    // Shutdown logics
     @Override
     public void onDisable() {
         getLogger().info("AntiElytraLag disabled!");
+    }
+    // Debug commands
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("ael")) {
+            PluginDescriptionFile pdf = this.getDescription();
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&3⌛&8] &7AntiElytraLag " + pdf.getVersion() + " is running."));
+
+        }
+        return false;
     }
 }
